f9ff8b563029e8896e4ee75d7c5883942a318a9a
Olu Gbadebo (Student) <odgbadeb@sundevils.asu.edu>
2018-11-14 11:28:54 -0700

    implementing gridding for section one

--- MesaCity.js
+++ MesaCity.js
@@ -1,12 +1,13 @@
 const Coord = require('./utils').Coord;
 
-function CitySection () {
+function CitySection (name) {
+    this.name = name;
     this.northEast = new Coord(null, null);
     this.southEast = new Coord(null, null);
     this.northWest = new Coord(null, null);
     this.southWest = new Coord(null, null);
     this.log = () => {
-        console.log("NorthEast: ");
+        console.log("Name: %s, \nNorthEast: ", this.name);
         this.northEast.log();
         console.log("NorthWest: ");
         this.northWest.log();
@@ -21,31 +22,36 @@ let singleInstance = null;
 
 function MesaCity() {
     singleInstance = this;
-    let sectionOne = new CitySection();
-    sectionOne.northEast = new Coord(33.455611, -111.840738);
-    sectionOne.northWest = new Coord(33.455042, -111.889525);
-    sectionOne.southWest = new Coord(33.357027, -111.893905);
+    let sectionOne = new CitySection("sectionOne");
+    sectionOne.northEast = new Coord(33.455611, -111.841803);
+    sectionOne.northWest = new Coord(33.455611,-111.893905);
+    sectionOne.southWest = new Coord(33.357090, -111.893905);
     sectionOne.southEast = new Coord(33.357090, -111.841803);
 
-    let sectionTwo = new CitySection();
+    let sectionTwo = new CitySection("sectionTwo");
     sectionTwo.northEast = new Coord(33.455611, -111.688848);
-    sectionTwo.northWest = new Coord(33.455611, -111.840738);
-    sectionTwo.southWest = new Coord(33.357090, -111.841803);
+    sectionTwo.northWest = new Coord(33.455611, -111.841803);
+    sectionTwo.southWest = new Coord(33.379279, -111.841803);
     sectionTwo.southEast = new Coord(33.379279, -111.688848);
 
-    let sectionThree = new CitySection();
+    let sectionThree = new CitySection("sectionThree");
     sectionThree.northEast = new Coord(33.516079, -111.632890);
     sectionThree.northWest = new Coord(33.516079, -111.840738);
-    sectionThree.southWest = new Coord(33.455611, -111.840738);
+    sectionThree.southWest = new Coord(33.468072, -111.840738);
     sectionThree.southEast = new Coord(33.468072, -111.632890);
 
-    let sectionFour = new CitySection();
-    sectionFour.northEast = new Coord(33.455611, -111.581634);
+    let sectionFour = new CitySection("sectionFour");
+    sectionFour.northEast = new Coord(33.455611, -111.584269);
     sectionFour.northWest = new Coord(33.455611, -111.688848);
-    sectionFour.southWest = new Coord(33.278884, -111.686500);
+    sectionFour.southWest = new Coord(33.282113, -111.688848);
     sectionFour.southEast = new Coord(33.282113, -111.584269);
 
-    this.sections = [sectionOne, sectionTwo, sectionThree, sectionFour]
+    this.sections = {
+        sectionOne: sectionOne,
+        sectionTwo: sectionTwo,
+        sectionThree: sectionThree,
+        sectionFour: sectionFour,
+    };
 }
 
 function getCity() {
