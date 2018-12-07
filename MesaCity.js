ff3c3a029c9339c0bc190ba9c04ebf05366ff22d
Olu Gbadebo <odgbadeb@asu.edu>
2018-12-06 18:49:16 -0700

    sections are more informational

--- MesaCity.js
+++ MesaCity.js
@@ -6,6 +6,32 @@ function CitySection (name) {
     this.southEast = new Coord();
     this.northWest = new Coord();
     this.southWest = new Coord();
+    this.rows = [];
+    this.customerCount = 0;
+    this.gridCount = 0;
+
+    this.density = () => {
+        let density = 0;
+        for (let row of this.rows) {
+            for (let grid of row.grids) {
+                density += grid.density()
+            }
+        }
+        return density;
+    };
+    this.toArray = function() {
+        return [
+            this.southWest.toArray(),
+            this.northWest.toArray(),
+            this.northEast.toArray(),
+            this.southEast.toArray()
+        ];
+    };
+    this.toBlockArray = () => {
+        let result = this.toArray();
+        result.push(this.southWest.toArray());
+        return result;
+    };
     this.log = () => {
         console.log("Name: %s, \nNorthEast: ", this.name);
         this.northEast.log();
@@ -46,6 +72,59 @@ function MesaCity() {
     sectionFour.southWest = new Coord(33.282113, -111.688848);
     sectionFour.southEast = new Coord(33.282113, -111.584269);
 
+    this.findSection = coord => {
+        if (!(coord instanceof Coord)) {
+            console.error("findSection: Type of coord must be utils/Coord");
+            return
+        }
+
+        function isBoundedByTopAndBottom(section) {
+            return section.northWest.lat >= coord.lat && section.southWest.lat <= coord.lat;
+        }
+
+        function isBoundedByLeftAndRight(section) {
+            return section.northEast.lng >= coord.lng && section.northWest.lng <= coord.lng;
+        }
+
+        return Object.values(this.sections).find(eachSection => {
+            return isBoundedByTopAndBottom(eachSection) && isBoundedByLeftAndRight(eachSection);
+        })
+    };
+
+    this.findRow = (rows, coord) => {
+        return rows.find(eachRow => {
+            function isBoundedByTopAndBottom() {
+                return eachRow.bounds.northWest.lat >= coord.lat && eachRow.bounds.southWest.lat <= coord.lat;
+            }
+
+            function isBoundedByLeftAndRight() {
+                return eachRow.bounds.northEast.lng >= coord.lng && eachRow.bounds.northWest.lng <= coord.lng;
+            }
+
+            return isBoundedByTopAndBottom() && isBoundedByLeftAndRight();
+        });
+    };
+
+    this.findGrid = coord => {
+        let section = this.findSection(coord);
+        if (section === undefined) return section;
+        let row = this.findRow(section.rows, coord);
+        if (row === undefined) return row;
+        return row.grids.find(eachGrid => {
+            return eachGrid.contains(coord);
+        })
+    };
+
+    this.resetGridPickups = () => {
+        for (let section of Object.values(this.sections)) {
+            for (let row of section.rows) {
+                for (let grid of row.grids) {
+                    grid.pickups = 0;
+                }
+            }
+        }
+    };
+
     this.sections = {
         sectionOne: sectionOne,
         sectionTwo: sectionTwo,
@@ -54,10 +133,4 @@ function MesaCity() {
     };
 }
 
-function getCity() {
-    return singleInstance ? singleInstance : new MesaCity();
-}
-
-module.exports = {
-    sections: getCity().sections
-};
+module.exports = singleInstance ? singleInstance : new MesaCity();
