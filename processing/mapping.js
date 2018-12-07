e103766d0eadae8b4cf78f478031d8b4175b12c4
Olu Gbadebo <odgbadeb@asu.edu>
2018-12-06 18:50:26 -0700

    implemented mapping customers to grid

--- processing/mapping.js
+++ processing/mapping.js
@@ -1,7 +1,27 @@
 // map data to grid
+const MesaCity = require('../MesaCity');
+const dataProvider = require('../data/provider');
+const Coord = require('../utils').Coord;
 
-const main = () => {
-//    get grid from preprocess.js
-//    map data in excel/csv to grid
-//    return mapping
+async function mapCustomersToGrids () {
+    await dataProvider.getFromFile('./data/allcustomers.csv', ['latitude', 'longitude'])
+        .then(data => {
+            for (let customer of data) {
+                if (customer && customer.hasOwnProperty('LATITUDE') && customer.hasOwnProperty('LONGITUDE')) {
+                    let location = new Coord(customer.LATITUDE, customer.LONGITUDE);
+                    //FIXME: returns undefined for valid location
+                    let grid = MesaCity.findGrid(location);
+                    if (grid) {
+                        grid.customers++;
+                    }
+                }
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+module.exports = {
+    customersToGrid: mapCustomersToGrids
 };
