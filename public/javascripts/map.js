8033faddcaea313629612dc3a9bed86ab6644524
Olu Gbadebo <odgbadeb@asu.edu>
2019-02-21 18:08:12 -0700

    supports adding multiple maps to the page

---
+++ public/javascripts/map.js
@@ -0,0 +1,31 @@
+/**
+    Create a single instance of leaflet map and attaches the map to a div#map element.
+    @param {object} options:
+        @property {string} divID: ID of div to attach map to
+ */
+function initMap(options) {
+    let map;
+    if (options.divID) map = L.map(options.divID).fitWorld();
+    else return mapconsole.error('Map div is not provided.');
+
+    let tileUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoib2RnYmFkZWIiLCJhIjoiY2pvb2pjcnVjMW10aDNrbnZ2cnh4dWFieCJ9.3-pk14uWdAopXCwSGYMonw';
+    let tileLayerOptions = {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1Ijoib2RnYmFkZWIiLCJhIjoiY2pvb2pjcnVjMW10aDNrbnZ2cnh4dWFieCJ9.3-pk14uWdAopXCwSGYMonw'
+    };
+
+    L.tileLayer(tileUrl, tileLayerOptions).addTo(map);
+
+    //  restrict zoom out to city level
+    map.setMinZoom(11.4);
+
+    //  restrict draggable map to city bounds
+    let MesaCityNECorner = L.latLng(33.516079, -111.841803);
+    let MesaCitySWCorner = L.latLng(33.282113, -111.584269);
+    let bounds = L.latLngBounds(MesaCityNECorner, MesaCitySWCorner);
+    map.setMaxBounds(bounds);
+
+    return map;
+}
---
+++ public/javascripts/trucksMapInteraction.js
@@ -0,0 +1,1 @@
+initMap({divID: 'trucks_map'});
