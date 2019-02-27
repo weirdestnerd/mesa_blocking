3ca6e3394723b49357ceea71bc98173f0dd0438e
Olu Gbadebo <odgbadeb@asu.edu>
2019-02-26 17:59:08 -0700

    handle trucks data rendering on map

--- public/javascripts/trucksMapInteraction.js
+++ public/javascripts/trucksMapInteraction.js
@@ -1,1 +1,9 @@
-initMap({divID: 'trucks_map'});
+(function loadMapLayout() {
+    densityMap = initMap({divID: 'trucks_map'});
+    mapconsole.message('Getting Trucks Map Data ...');
+    getTrucksGeoJSON()
+        .then(data => {
+        //    TODO: convert json to geoJSON format => {type: 'feature', properties: {}}
+        })
+        .catch(mapconsole.error)
+}());
