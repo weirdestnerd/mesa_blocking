30b1f31ec2fe8bf9ab6beb5816193a0257d43451
Olu Gbadebo <odgbadeb@asu.edu>
2019-03-06 20:18:24 -0700

    added graph for cans

--- public/javascripts/trucksMapInteraction.js
+++ public/javascripts/trucksMapInteraction.js
@@ -71,6 +71,65 @@ function createMapRouteLegend() {
     legend.addTo(trucksMap);
 }
 
+function calculateCansCount(data) {
+    let result = [];
+
+    for (let week in data) {
+        data[week].routes.forEach(truck => {
+            result.push({'vehicle': truck.vehicle, 'cans': truck.cans})
+        })
+    }
+    return result;
+}
+
+function createCansGraph(data) {
+    let cansCount = calculateCansCount(data);
+    let margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 320 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
+
+// set the ranges
+    let x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    let y = d3.scaleLinear()
+        .range([height, 0]);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+    let svg = d3.select("div.cans_graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data in the domains
+    x.domain(cansCount.map(function(d) { return d.vehicle; }));
+    y.domain([0, d3.max(cansCount, function(d) { return d.cans; })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(cansCount)
+        .enter().append("rect")
+        .attr("class", "cans bar")
+        .attr("x", function(d) { return x(d.vehicle); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.cans); })
+        .attr("height", function(d) { return height - y(d.cans); });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+}
+
 (function loadMapLayout() {
     trucksMap = initMap({divID: 'trucks_map'});
     mapconsole.message('Getting Trucks Map Data ...');
@@ -91,6 +150,11 @@ function createMapRouteLegend() {
                 addSelectionListenerToTrucksWeek(week, allWeeksButton);
             }
             createMapRouteLegend();
+            return data;
         })
+        .then(data => {
+            createCansGraph(data);
+        })
+        .catch(mapconsole.error)
         .catch(mapconsole.error)
 }());
